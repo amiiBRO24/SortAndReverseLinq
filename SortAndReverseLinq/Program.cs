f9086aa1fb3abb285b15a57bcbd733af8f80e05a
Neil Gonzalez <nerdcoregames.studio@gmail.com>
2021-07-20 19:52:54 -0400

    Added sort and reverse linq code

--- SortAndReverseLinq/Program.cs
+++ SortAndReverseLinq/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SortAndReverseLinq
 {
@@ -6,7 +10,23 @@ namespace SortAndReverseLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] someInt = { 30, 12, 4, 3, 12 };
+
+            IEnumerable<int> sortedInts = from i in someInt orderby i select i;
+            IEnumerable<int> reversedInts = sortedInts.Reverse();
+
+            foreach (int i in reversedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            //This is to descending
+            IEnumerable<int> reversedSortedInts = from i in someInt orderby i descending select i;
+
+            foreach (int i in reversedSortedInts)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
